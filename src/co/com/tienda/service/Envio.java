a7f0f82fc48279d76aef13ee07a74940144403b7
Jhoan Oswaldo Ome Vega <jhoanoswaldo72@gmail.com>
2023-02-13 21:53:31 -0500

    version final

--- src/co/com/tienda/service/Envio.java
+++ src/co/com/tienda/service/Envio.java
@@ -3,9 +3,9 @@ package co.com.tienda.service;
 import co.com.tienda.modelo.User;
 
 public class Envio implements ConnectionBackend {
-    public void sendProducto(User usuario)
+    public void sendProducto(User usuario)//Informa al usuario sobre su compra, se "procesa" y se envia
     {
-        System.out.println(usuario.getName()+", gracias por comprar en nuestra tienda!!");
+        System.out.println(usuario.getName()+", su compra se ha realizado y sera enviada al "+usuario.getAddress());
     }
 
     @Override
