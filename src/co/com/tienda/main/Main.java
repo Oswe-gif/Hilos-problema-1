32692c9511a01083938166f4c6dece6503f34f5e
Jhoan Oswaldo Ome Vega <jhoanoswaldo72@gmail.com>
2023-02-13 22:13:59 -0500

    versionFinalT

--- src/co/com/tienda/main/Main.java
+++ src/co/com/tienda/main/Main.java
@@ -7,6 +7,7 @@ import co.com.tienda.service.Service;
 
 public class Main {
     public static void main(String[] args) {
+        //realiza una compra
         User user1 = new User("Samuel","Zapatos","Conjunto Santa Ana");
         Service service = new Service(new MySQL(),new Envio());//connect with the backend and the bd
         Thread newThread= new Thread(service);
