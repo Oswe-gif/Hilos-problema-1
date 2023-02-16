93bc3e5b59c4247b40dc13d50c19cbbcbef13723
Jhoan Oswaldo Ome Vega <jhoanoswaldo72@gmail.com>
2023-02-15 20:15:47 -0500

    versionActualizadaFinal

--- src/co/com/tienda/main/Main.java
+++ src/co/com/tienda/main/Main.java
@@ -1,7 +1,7 @@
 package co.com.tienda.main;
 
 import co.com.tienda.modelo.User;
-import co.com.tienda.service.Envio;
+import co.com.tienda.service.Sender;
 import co.com.tienda.service.MySQL;
 import co.com.tienda.service.Service;
 
@@ -9,11 +9,11 @@ public class Main {
     public static void main(String[] args) {
         //realiza una compra
         User user1 = new User("Samuel","Zapatos","Conjunto Santa Ana");
-        Service service = new Service(new MySQL(),new Envio());//connect with the backend and the bd
+        Service service = new Service(new MySQL(),new Sender());//connect with the backend and the bd
         Thread newThread= new Thread(service);
         newThread.start();
-        Envio envio = new Envio();
-        envio.sendProducto(user1);
+        Sender sender = new Sender();
+        sender.sendProducto(user1);
 
     }
 }
--- src/co/com/tienda/service/Sender.java
+++ src/co/com/tienda/service/Sender.java
@@ -2,7 +2,7 @@ package co.com.tienda.service;
 
 import co.com.tienda.modelo.User;
 
-public class Envio implements ConnectionBackend {
+public class Sender implements ConnectionBackend {
     public void sendProducto(User usuario)//Informa al usuario sobre su compra, se "procesa" y se envia
     {
         System.out.println(usuario.getName()+", su compra se ha realizado y sera enviada al "+usuario.getAddress());
